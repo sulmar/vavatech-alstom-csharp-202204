4538c06462be3b2f34b73740321bd9314fb7b51a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-04-13 15:33:59 +0200

    Day 3

---
+++ Fans/Fan.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fans
+{
+    class Fan
+    { 
+        public string _model;
+        public string _color;
+
+        private int rpm;
+        private bool oscillation;
+
+        private const byte minLevel = 0;
+        private const byte maxLevel = 3;
+
+        public Fan(string model)
+        {
+            _model = model;
+            _color = "White";
+
+            rpm = 0;
+            oscillation = false;
+        }
+
+        private void SetRpm(byte level)
+        {
+            if (level >= minLevel && level <= maxLevel)
+            {
+                rpm = level * 100;
+            }
+        }
+
+        public void PushOscillationButton()
+        {
+            oscillation = !oscillation;
+        }
+
+        public void PushButton0()
+        {
+            SetRpm(0);
+        }
+
+        public void PushButton1()
+        {
+            SetRpm(1);
+        }
+
+        public void PushButton2()
+        {
+            SetRpm(2);
+        }
+
+        public void PushButton3()
+        {
+            SetRpm(3);
+        }
+
+        public void SetVolume(byte volume)
+        {
+
+        }
+
+
+    }
+}
---
+++ Fans/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan = new Fan("Vesta");
+            fan.PushButton2();
+            fan.PushButton0();
+
+            fan.PushOscillationButton();
+        }
+    }
+}
